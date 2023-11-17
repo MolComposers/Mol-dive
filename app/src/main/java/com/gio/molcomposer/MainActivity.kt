e27b1901cdcd66363489763ee0fa0b1d42882959
giovannijunseokim <kingjunseo@gmail.com>
2023-11-17 23:14:13 +0900

    리스트, LazyColumn

--- app/src/main/java/com/gio/molcomposer/MainActivity.kt
+++ app/src/main/java/com/gio/molcomposer/MainActivity.kt
@@ -4,18 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,19 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ModernComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    Column(
-                        modifier = Modifier
-                            .background(color = Color.Blue)
-                            .padding(16.dp)
-                            .fillMaxHeight()
-                    ) {
-                        Text("Hello")
-                    }
-                }
-            }
+            ModernCompose()
         }
     }
 }
@@ -46,20 +27,22 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun Preview() {
+    ModernCompose()
+}
+
+@Composable
+fun ModernCompose() {
     ModernComposeTheme {
-        // A surface container using the 'background' color from the theme
-        Box(
+
+        LazyColumn(
             modifier = Modifier
-                .background(color = Color.Green)
-                .fillMaxWidth()
-                .height(200.dp)
+                .background(color = Color.Red),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Text("Hello")
-            Box(
-                modifier = Modifier.fillMaxSize().padding(16.dp),
-                contentAlignment = Alignment.BottomEnd
-            ) {
-                Text("Gio")
+            item { Text("Header") }
+            items(50) { index ->
+                Text("$index 번째 Text")
             }
         }
     }
