b8ccf4bdc548c3121569e4c9e24d56e7812ff988
giovannijunseokim <kingjunseo@gmail.com>
2023-11-17 23:14:42 +0900

    Image, Card, State

--- app/src/main/java/com/gio/molcomposer/MainActivity.kt
+++ app/src/main/java/com/gio/molcomposer/MainActivity.kt
@@ -3,14 +3,30 @@ package com.gio.molcomposer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gio.molcomposer.ui.theme.ModernComposeTheme
@@ -33,16 +49,47 @@ fun Preview() {
 @Composable
 fun ModernCompose() {
     ModernComposeTheme {
+        var isFavorite by rememberSaveable {
+            mutableStateOf(false)
+        }
 
-        LazyColumn(
+        ImageCard(
             modifier = Modifier
-                .background(color = Color.Red),
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            item { Text("Header") }
-            items(50) { index ->
-                Text("$index 번째 Text")
+                .fillMaxWidth(0.5f)
+                .padding(16.dp), isFavorite
+        ) { newIsFavorite ->
+            isFavorite = newIsFavorite
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    modifier: Modifier = Modifier, isFavorite: Boolean, onClickFavorite: (Boolean) -> Unit
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        )
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.botji),
+                contentDescription = "사진",
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd
+            ) {
+                IconButton(onClick = { onClickFavorite(!isFavorite) }) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "좋아요 버튼",
+                        tint = Color.White
+                    )
+                }
             }
         }
     }
