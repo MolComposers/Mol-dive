fec2b3c92f13e756c12c05f02f6bc10bd1f1a0dd
giovannijunseokim <kingjunseo@gmail.com>
2023-11-18 04:31:05 +0900

    Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -70,4 +70,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.ui:ui:1.5.4")
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
 }
--- app/src/main/java/com/gio/molcomposer/MainActivity.kt
+++ app/src/main/java/com/gio/molcomposer/MainActivity.kt
@@ -5,25 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.gio.molcomposer.ui.theme.ModernComposeTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,36 +39,85 @@ fun Preview() {
     ModernCompose()
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ModernCompose() {
     ModernComposeTheme {
-        val (text, setValue) = remember {
-            mutableStateOf("")
-        }
-        val snackbarHostState = remember { SnackbarHostState() }
-        val scope = rememberCoroutineScope()
-        val keyboardController = LocalSoftwareKeyboardController.current
-        Scaffold(
-            snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
-        ) { contentPadding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(contentPadding),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                TextField(value = text, onValueChange = setValue)
-                Button(onClick = {
-                    scope.launch {
-                        snackbarHostState.showSnackbar("Hello $text")
-                    }
-                    keyboardController?.hide()
-                }) {
-                    Text(text = "클릭")
-                }
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController, startDestination = "first"
+        ) {
+            composable("first") {
+                FirstScreen(navController)
             }
+            composable("second") {
+                SecondScreen(navController)
+            }
+            composable("third/{value}") { backStackEntry ->
+                ThirdScreen(
+                    navController = navController,
+                    value = backStackEntry.arguments?.getString("value") ?: ""
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen(navController: NavHostController) {
+    val (value, setValue) = remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "첫 화면")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate(route = "second") }) {
+            Text(text = "두 번째!")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(value = value, onValueChange = setValue)
+        Button(onClick = {
+            if (value.isNotEmpty()) {
+                navController.navigate(route = "third/$value")
+            }
+        }) {
+            Text(text = "세 번째!")
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "두 번째 화면")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            navController.navigateUp()
+        }) {
+            Text(text = "뒤로 가기")
+        }
+    }
+}
+
+@Composable
+fun ThirdScreen(navController: NavHostController, value: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "세 번째 화면")
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = value)
+        Button(onClick = { navController.navigateUp() }) {
+            Text(text = "뒤로 가기")
         }
     }
 }
