95e629dd177fb847093e7255c1bb7c57055ff8bc
giovannijunseokim <kingjunseo@gmail.com>
2023-11-17 23:51:17 +0900

    Scaffold, TextField, Button, 구조분해 SnackBar, 코루틴 스코프

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.ui:ui:1.5.4")
 }
--- app/src/main/java/com/gio/molcomposer/MainActivity.kt
+++ app/src/main/java/com/gio/molcomposer/MainActivity.kt
@@ -3,33 +3,27 @@ package com.gio.molcomposer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.gio.molcomposer.ui.theme.ModernComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,49 +40,34 @@ fun Preview() {
     ModernCompose()
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ModernCompose() {
     ModernComposeTheme {
-        var isFavorite by rememberSaveable {
-            mutableStateOf(false)
+        val (text, setValue) = remember {
+            mutableStateOf("")
         }
-
-        ImageCard(
-            modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp), isFavorite
-        ) { newIsFavorite ->
-            isFavorite = newIsFavorite
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    modifier: Modifier = Modifier, isFavorite: Boolean, onClickFavorite: (Boolean) -> Unit
-) {
-    Card(
-        modifier = modifier,
-        shape = RoundedCornerShape(8.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 8.dp
-        )
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painterResource(id = R.drawable.botji),
-                contentDescription = "사진",
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd
+        val snackbarHostState = remember { SnackbarHostState() }
+        val scope = rememberCoroutineScope()
+        val keyboardController = LocalSoftwareKeyboardController.current
+        Scaffold(
+            snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        ) { contentPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(contentPadding),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                IconButton(onClick = { onClickFavorite(!isFavorite) }) {
-                    Icon(
-                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-                        contentDescription = "좋아요 버튼",
-                        tint = Color.White
-                    )
+                TextField(value = text, onValueChange = setValue)
+                Button(onClick = {
+                    scope.launch {
+                        snackbarHostState.showSnackbar("Hello $text")
+                    }
+                    keyboardController?.hide()
+                }) {
+                    Text(text = "클릭")
                 }
             }
         }
