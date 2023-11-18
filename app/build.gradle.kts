acadd6d22499a3ba570bd1c54f8e5c9f6f424e07
giovannijunseokim <kingjunseo@gmail.com>
2023-11-18 13:30:37 +0900

    ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,6 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.5.4")
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 }
--- app/src/main/java/com/gio/molcomposer/MainActivity.kt
+++ app/src/main/java/com/gio/molcomposer/MainActivity.kt
@@ -5,23 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.gio.molcomposer.ui.theme.ModernComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,83 +36,27 @@ fun Preview() {
 
 @Composable
 fun ModernCompose() {
+    val viewModel = viewModel<MainViewModel>()
     ModernComposeTheme {
-        val navController = rememberNavController()
-        NavHost(
-            navController = navController, startDestination = "first"
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            composable("first") {
-                FirstScreen(navController)
-            }
-            composable("second") {
-                SecondScreen(navController)
-            }
-            composable("third/{value}") { backStackEntry ->
-                ThirdScreen(
-                    navController = navController,
-                    value = backStackEntry.arguments?.getString("value") ?: ""
-                )
+            Text(text = viewModel.data.value, fontSize = 30.sp)
+            Button(onClick = {
+                viewModel.changeData("World")
+            }) {
+                Text(text = "변경")
             }
         }
     }
 }
 
-@Composable
-fun FirstScreen(navController: NavHostController) {
-    val (value, setValue) = remember {
-        mutableStateOf("")
-    }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "첫 화면")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate(route = "second") }) {
-            Text(text = "두 번째!")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(value = value, onValueChange = setValue)
-        Button(onClick = {
-            if (value.isNotEmpty()) {
-                navController.navigate(route = "third/$value")
-            }
-        }) {
-            Text(text = "세 번째!")
-        }
-    }
-}
-
-@Composable
-fun SecondScreen(navController: NavHostController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "두 번째 화면")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            navController.navigateUp()
-        }) {
-            Text(text = "뒤로 가기")
-        }
-    }
-}
-
-@Composable
-fun ThirdScreen(navController: NavHostController, value: String) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "세 번째 화면")
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = value)
-        Button(onClick = { navController.navigateUp() }) {
-            Text(text = "뒤로 가기")
-        }
+class MainViewModel : ViewModel() {
+    private val _data = mutableStateOf("Hello")
+    val data: State<String> = _data
+    fun changeData(newData: String) {
+        _data.value = newData
     }
 }
