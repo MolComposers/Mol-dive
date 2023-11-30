554f3a1c968a6fa04d409ea03aed2bac20222999
giovannijunseokim <kingjunseo@gmail.com>
2023-11-30 14:35:22 +0900

    feat: Section 2 - Second Screen

--- obesitycalculator/src/main/java/com/gio/obesitycalculator/MainActivity.kt
+++ obesitycalculator/src/main/java/com/gio/obesitycalculator/MainActivity.kt
@@ -1,14 +1,19 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.gio.obesitycalculator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -23,9 +28,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gio.obesitycalculator.ui.theme.ModernComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,16 +46,15 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    FirstScreen()
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FirstScreen() {
+fun HomeScreen() {
     val (height, setHeight) = rememberSaveable {
         mutableStateOf("")
     }
@@ -86,10 +94,43 @@ fun FirstScreen() {
     }
 }
 
+@Composable
+fun ResultScreen(bmi: Double) {
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = "비만도 계산기") })
+    }) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "과체중", fontSize = 30.sp)
+            Spacer(modifier = Modifier.height(50.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_face_dissatisfied),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp),
+                colorFilter = ColorFilter.tint(color = Color.Black)
+            )
+        }
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
-fun Preview() {
+fun FirstScreenPreview() {
     ModernComposeTheme {
-        FirstScreen()
+        HomeScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondScreenPreview() {
+    ModernComposeTheme {
+        ResultScreen(35.0)
     }
 }
