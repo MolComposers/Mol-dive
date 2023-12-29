d4a0cac458f092987018a30626c7faa1f56eb380
Giovanni Junseo Kim <kingjunseo@gmail.com>
2023-12-29 14:09:58 +0900

    Revert "Section/2 obesity calculator"

--- obesitycalculator/src/main/java/com/gio/obesitycalculator/MainActivity.kt
+++ obesitycalculator/src/main/java/com/gio/obesitycalculator/MainActivity.kt
@@ -1,48 +1,15 @@
-@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
-
 package com.gio.obesitycalculator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.tooling.preview.Preview
 import com.gio.obesitycalculator.ui.theme.ModernComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,23 +19,10 @@ class MainActivity : ComponentActivity() {
             ModernComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    val viewModel: MainViewModel = viewModel()
-                    val bmi = viewModel.bmi.value
-
-                    val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = "home") {
-                        composable(route = "home") {
-                            HomeScreen { height, weight ->
-                                viewModel.calculateBmi(height, weight)
-                                navController.navigate(route = "result")
-                            }
-                        }
-                        composable(route = "result") {
-                            ResultScreen(navController, bmi)
-                        }
-                    }
+                    Greeting("Android")
                 }
             }
         }
@@ -76,107 +30,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen(onResultBtnClick: (Double, Double) -> Unit) {
-    val (height, setHeight) = rememberSaveable {
-        mutableStateOf("")
-    }
-    val (weight, setWeight) = rememberSaveable {
-        mutableStateOf("")
-    }
-    Scaffold(topBar = {
-        TopAppBar(title = { Text(text = "비만도 계산기") })
-    }) {
-        Column(
-            modifier = Modifier
-                .padding(it)
-                .padding(16.dp)
-        ) {
-            OutlinedTextField(
-                value = height,
-                onValueChange = setHeight,
-                label = { Text(text = "키") },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-            )
-            OutlinedTextField(
-                value = weight,
-                onValueChange = setWeight,
-                label = { Text(text = "몸무게") },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Button(
-                onClick = {
-                    if (height.isNotBlank() && weight.isNotBlank()) onResultBtnClick(
-                        height.toDouble(), weight.toDouble()
-                    )
-                }, modifier = Modifier.align(Alignment.End)
-            ) {
-                Text(text = "결과")
-            }
-        }
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
+@Preview(showBackground = true)
 @Composable
-fun ResultScreen(navController: NavController, bmi: Double) {
-    val text = when {
-        bmi >= 35 -> "고도 비만"
-        bmi >= 30 -> "2단계 비만"
-        bmi >= 25 -> "1단계 비만"
-        bmi >= 23 -> "과체중"
-        bmi >= 18.5 -> "정상"
-        else -> "저체중"
-    }
-
-    val iconResId = when {
-        bmi >= 23 -> R.drawable.ic_face_very_dissatisfied
-        bmi >= 18.5 -> R.drawable.ic_face_satisfied
-        else -> R.drawable.ic_face_dissatisfied
-    }
-
-    Scaffold(topBar = {
-        TopAppBar(title = { Text(text = "비만도 계산기") }, navigationIcon = {
-            Icon(imageVector = Icons.Default.ArrowBack,
-                contentDescription = "뒤로 가기",
-                modifier = Modifier.clickable {
-                    navController.popBackStack()
-                })
-        })
-    }) {
-        Column(
-            modifier = Modifier
-                .padding(it)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = text, fontSize = 30.sp)
-            Spacer(modifier = Modifier.height(50.dp))
-            Image(
-                painter = painterResource(id = iconResId),
-                contentDescription = null,
-                modifier = Modifier.size(100.dp),
-                colorFilter = ColorFilter.tint(color = Color.Black)
-            )
-        }
+fun GreetingPreview() {
+    ModernComposeTheme {
+        Greeting("Android")
     }
 }
-
-
-//@Preview(showBackground = true)
-//@Composable
-//fun FirstScreenPreview() {
-//    ModernComposeTheme {
-//        HomeScreen(navController = NavController())
-//    }
-//}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun SecondScreenPreview() {
-//    ModernComposeTheme {
-//        ResultScreen(35.0)
-//    }
-//}
