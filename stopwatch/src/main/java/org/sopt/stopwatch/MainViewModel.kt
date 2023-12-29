aba6874fbdd0a5ea4823d282d53c12769557203f
giovannijunseokim <kingjunseo@gmail.com>
2023-12-29 14:46:15 +0900

    feat: MainViewModel init

---
+++ stopwatch/src/main/java/org/sopt/stopwatch/MainViewModel.kt
@@ -0,0 +1,12 @@
+package org.sopt.stopwatch
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import java.util.Timer
+import kotlin.concurrent.timer
+
+class MainViewModel : ViewModel() {
+
+}
